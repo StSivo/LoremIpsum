faf1e7669ad75b40f948da78be04ec8046a879a6
stsivo-apple <ssivo22@fed.idserve.net>
2023-02-22 12:50:39 +0100

    First commit

---
+++ LoremIpsum/LoremIpsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremIpsumApp.swift
+//  LoremIpsum
+//
+//  Created by Stefano Sivo on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LoremIpsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
